9c8f99bac91f1254b43f39d9b83cc882cfd8b32a
Jordan Herget <jordan.herget@jobnimbus.com>
2022-07-09 12:35:39 -0600

    display the mandelbrot set and translate to where the user clicks

--- main.py
+++ main.py
@@ -4,21 +4,44 @@ import numpy as np
 import math
 import random
 
-res = 20
+res = 4
 width, height = 1200, 800
-ROW, COL = height // res, width // res
-range_x, range_y = (-2, 2), (-1.5, 1.5)
+ROW, COL = height // res, height // res
+zoom, center = 2, (-0.5, 0)
+range_x, range_y = (center[0] - zoom//2, center[0] + zoom//2), (center[1] - zoom//2, center[1] + zoom//2)
 
 root = Window(width, height, "Exploring the Mandelbrot Set")
 canvas = root.add_canvas(height, height, 0, 0)
 
 display = np.zeros((ROW, COL), dtype=np.uint8)
-C = complex(1/3, 0)
+canvas_display = np.zeros((height, height, 3), dtype=np.uint8)
+image = ImageTk.PhotoImage(image=Image.fromarray(canvas_display))
+
+def map_range(num, num_lower, num_higher, new_lower, new_higher):
+    return (num - num_lower) * (new_higher - new_lower) / (num_higher - num_lower) + new_lower
+
+def translate_display(event):
+    global zoom, center, range_x, range_y
+
+    x = map_range(event.x, 0, height, range_x[0], range_x[1])
+    y = map_range(event.y, 0, height, range_y[0], range_y[1])
+
+    center = (x, y)
+    range_x, range_y = (center[0] - zoom // 2, center[0] + zoom // 2), (center[1] - zoom // 2, center[1] + zoom // 2)
+
+    _display = calculate_escape_values(display)
+    image = colorize_display(_display)
+    # _image = ImageTk.getimage(image)
+    # _image.save("screenshot.png")
 
 def f(Z, C):
     return Z**2 + C
 
-def calc_f(Z, C):
+def calc_f(row_col_elem):
+    (row, col), elem = row_col_elem
+    C_real = map_range(col, 0, COL - 1, range_x[0], range_x[1])
+    C_imag = map_range(row, 0, ROW - 1, range_y[0], range_y[1])
+    Z, C = complex(0, 0), complex(C_real, C_imag)
     infinity = 10
 
     escape_value = f(Z, C)
@@ -30,25 +53,25 @@ def calc_f(Z, C):
 
     return 200
 
-def map(num, num_lower, num_higher, new_lower, new_higher):
-    return (num - num_lower) * (new_higher - new_lower) / (num_higher - num_lower) + new_lower
+def calculate_escape_values(display):
+    return np.reshape(list(map(calc_f, np.ndenumerate(display))), (ROW, COL))
 
-for j, row in enumerate(display):
-    for i, col in enumerate(row):
-        C_real = map(i, 0, COL - 1, range_x[0], range_x[1])
-        C_imag = map(j, 0, ROW - 1, range_y[0], range_y[1])
-        escape_value = calc_f(complex(0), complex(C_real, C_imag))
-        display[j][i] = escape_value
+def colorize_display(display):
+    global image, canvas_display
 
-canvas_display = np.zeros((height, height, 3), dtype=np.uint8)
-for j, row in enumerate(display):
-    for i, col in enumerate(row):
-        escape_value = display[j][i]
-        color = np.array([escape_value, escape_value, escape_value], dtype=np.uint8)
-        for k in range(res):
-            canvas_display[j*res + k][i*res:(i + 1)*res] = color
+    for j, row in enumerate(display):
+        for i, col in enumerate(row):
+            escape_value = display[j][i]
+            color = np.array([escape_value, escape_value, escape_value], dtype=np.uint8)
+            for k in range(res):
+                canvas_display[j*res + k][i*res:(i + 1)*res] = color
 
-image = ImageTk.PhotoImage(image=Image.fromarray(canvas_display))
-canvas.create_image(height//2, height//2, image=image)
+    image = ImageTk.PhotoImage(image=Image.fromarray(canvas_display))
+    canvas.delete("all")
+    canvas.create_image(height // 2, height // 2, image=image)
 
+display = calculate_escape_values(display)
+colorize_display(display)
+
+canvas.bind("<Button>", translate_display)
 root.mainloop()
